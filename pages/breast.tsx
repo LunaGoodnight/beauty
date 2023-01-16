106c4e49448b0fe04977552cc1268627315f7585
Luna Goodnight <luna.lee@nextfortune.tw>
2023-01-16 15:53:09 +0800

    [Add] npmrc

--- pages/breast.tsx
+++ pages/breast.tsx
@@ -1,9 +1,11 @@
 import { Header } from "../common/Header";
+import {LeftNav} from "../common/LeftNav";
 
 const breast = () => {
   return (
     <div>
       <Header />
+      <LeftNav />
       胸部大大的
     </div>
   );
